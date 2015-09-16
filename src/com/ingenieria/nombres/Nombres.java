b1e8bf5abd162270c11bc462cab95399c7d262ec
Marcos <Marcos@marcos>
2015-09-16 12:18:04 -0600

    Agregue Marcos a la clase Nombres

---
+++ src/com/ingenieria/nombres/Nombres.java
@@ -0,0 +1,12 @@
+package com.ingenieria.nombres;
+
+import javax.swing.JOptionPane;
+
+public class Nombres {
+    
+    public static void main(String[] args)
+    {
+        JOptionPane.showMessageDialog(null, "Marcos");
+    }
+    
+}
